5f7a27160b6a7bbfc19d9d50cba032347e1fb64c
Nhat-Thanh <luonkebenban@gmail.com>
2022-05-13 06:40:15 +0700

    add test.py

--- test.py
+++ test.py
@@ -3,57 +3,52 @@ from model import VDSR
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--scale",     type=int, default=2,  help='-')
-parser.add_argument("--ckpt-path", type=str, default="", help='-')
+parser.add_argument('--scale',        type=int,   default=2,                   help='-')
+parser.add_argument("--ckpt-path",    type=str,   default="",                  help='-')
+parser.add_argument("--image-path",   type=str,   default="dataset/test1.png", help='-')
 
-FLAGS, _ = parser.parse_known_args()
+FLAGS, unparsed = parser.parse_known_args()
+image_path = FLAGS.image_path
 
 scale = FLAGS.scale
+if scale not in [2, 3, 4]:
+    ValueError("must be 2, 3 or 4")
+
 ckpt_path = FLAGS.ckpt_path
-if ckpt_path == "":
-    ckpt_path = "checkpoint/VDSR.pt"
+if (ckpt_path == "") or (ckpt_path == "default"):
+    ckpt_path = f"checkpoint/VDSR-{scale}.pt"
+
+sigma = 0.3 if scale == 2 else 0.2
 
 
 # -----------------------------------------------------------
-# load VDSR model 
+# demo
 # -----------------------------------------------------------
 
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
-model = VDSR(device)
-model.load_weights(ckpt_path)
+def main():
+    device = "cuda" if torch.cuda.is_available() else "cpu"
 
+    lr_image = read_image(image_path)
+    bicubic_image = upscale(lr_image, scale)
+    write_image("bicubic.png", bicubic_image)
 
-# -----------------------------------------------------------
-# init list of images
-# -----------------------------------------------------------
+    lr_image = gaussian_blur(lr_image, sigma=sigma)
+    bicubic_image = upscale(lr_image, scale)
+    bicubic_image = rgb2ycbcr(bicubic_image)
+    bicubic_image = norm01(bicubic_image)
+    bicubic_image = torch.unsqueeze(bicubic_image, dim=0)
 
-ls_data = sorted_list(f"dataset/test/x{scale}/data")
-ls_labels = sorted_list(f"dataset/test/x{scale}/labels")
-
-
-# -----------------------------------------------------------
-# preprocess and testing
-# -----------------------------------------------------------
-
-sum_psnr = 0
-with torch.no_grad():
-    for i in range(0, len(ls_data)):
-        hr_image = read_image(ls_labels[i])
-        lr_image = read_image(ls_data[i])
-        bicubic_image = upscale(lr_image, scale)
-
-        hr_image = rgb2ycbcr(hr_image)
-        bicubic_image = rgb2ycbcr(bicubic_image)
-
-        hr_image = norm01(hr_image)
-        bicubic_image = norm01(bicubic_image)
-
+    model = VDSR(device)
+    model.load_weights(ckpt_path)
+    with torch.no_grad():
         bicubic_image = bicubic_image.to(device)
-        bicubic_image = torch.unsqueeze(bicubic_image, dim=0)
         sr_image = model.predict(bicubic_image)[0]
-        sr_image = sr_image.cpu()
 
-        sum_psnr += PSNR(hr_image, sr_image, max_val=1)
+    sr_image = denorm01(sr_image)
+    sr_image = sr_image.type(torch.uint8)
+    sr_image = ycbcr2rgb(sr_image)
 
-print(sum_psnr / len(ls_data))
+    write_image("sr.png", sr_image)
 
+if __name__ == "__main__":
+    main()
