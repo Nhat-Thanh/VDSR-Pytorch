a770ccae441c222cd3a6751467f73250085fe87e
Nhat-Thanh <luonkebenban@gmail.com>
2022-05-13 06:43:22 +0700

    add test.py

--- test.py
+++ test.py
@@ -5,10 +5,8 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument('--scale',        type=int,   default=2,                   help='-')
 parser.add_argument("--ckpt-path",    type=str,   default="",                  help='-')
-parser.add_argument("--image-path",   type=str,   default="dataset/test1.png", help='-')
 
 FLAGS, unparsed = parser.parse_known_args()
-image_path = FLAGS.image_path
 
 scale = FLAGS.scale
 if scale not in [2, 3, 4]:
@@ -22,33 +20,37 @@ sigma = 0.3 if scale == 2 else 0.2
 
 
 # -----------------------------------------------------------
-# demo
+# test 
 # -----------------------------------------------------------
 
 def main():
     device = "cuda" if torch.cuda.is_available() else "cpu"
-
-    lr_image = read_image(image_path)
-    bicubic_image = upscale(lr_image, scale)
-    write_image("bicubic.png", bicubic_image)
-
-    lr_image = gaussian_blur(lr_image, sigma=sigma)
-    bicubic_image = upscale(lr_image, scale)
-    bicubic_image = rgb2ycbcr(bicubic_image)
-    bicubic_image = norm01(bicubic_image)
-    bicubic_image = torch.unsqueeze(bicubic_image, dim=0)
-
     model = VDSR(device)
     model.load_weights(ckpt_path)
+
+    ls_data = sorted_list(f"dataset/test/x{scale}/data")
+    ls_labels = sorted_list(f"dataset/test/x{scale}/labels")
+
+    sum_psnr = 0
     with torch.no_grad():
-        bicubic_image = bicubic_image.to(device)
-        sr_image = model.predict(bicubic_image)[0]
+        for i in range(0, len(ls_data)):
+            lr_image = read_image(ls_data[i])
+            lr_image = gaussian_blur(lr_image, sigma=sigma)
+            bicubic_image = upscale(lr_image, scale)
+            hr_image = read_image(ls_labels[i])
 
-    sr_image = denorm01(sr_image)
-    sr_image = sr_image.type(torch.uint8)
-    sr_image = ycbcr2rgb(sr_image)
+            bicubic_image = rgb2ycbcr(bicubic_image)
+            hr_image = rgb2ycbcr(hr_image)
 
-    write_image("sr.png", sr_image)
+            bicubic_image = norm01(bicubic_image)
+            hr_image = norm01(hr_image)
+
+            bicubic_image = torch.unsqueeze(bicubic_image, dim=0).to(device)
+            sr_image = model.predict(bicubic_image)[0].cpu()
+
+            sum_psnr += PSNR(hr_image, sr_image, max_val=1)
+
+    print(sum_psnr.numpy() / len(ls_data))
 
 if __name__ == "__main__":
     main()
